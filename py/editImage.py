80899986500cb35bff925a6ff96c584ef6c1a3a6
Floreny <floreny@IES-loaner01.local>
2017-10-09 21:57:53 +0200

    is centered, but not that nice

--- py/editImage.py
+++ py/editImage.py
@@ -1,13 +1,18 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import textwrap
 
 img = Image.open("../img/white.jpg")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
-font = ImageFont.truetype("Arial.ttf", 16)
+font = ImageFont.truetype("Arial.ttf", 30)
+W, H = (1080,1080)
+msg = "Mentors are great, but some of the greatest mentors are no longer alive"
+msg = msg.upper()
+msg = textwrap.fill(msg,20)
+w, h = draw.textsize(msg)
+draw.text(((W-w)/2,(H-h)/2), msg, fill="black",font=font)
 # draw.text((x, y),"Sample Text",(r,g,b))
-draw = ImageDraw.Draw(img)
-draw.text(xy=(50,50),text="moin moin", fill=(0,0,0),font=font)
 img.save('../img/output.jpg')
 img.show()
