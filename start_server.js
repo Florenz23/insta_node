d7d75e42a0834dc1ce8bc66ce5e77e31f1cdce87
Floreny <floreny@IES-loaner01.local>
2017-10-09 20:45:56 +0200

    upload is working

--- start_server.js
+++ start_server.js
@@ -12,20 +12,27 @@ Instagram.getCsrfToken().then((csrf) =>
   Instagram.auth('realflorenzerstling', 'SAFREIIY').then(sessionId =>
   {
     Instagram.sessionId = sessionId
+    console.log(Instagram)
+    Instagram.Upload.photo(session, './img/test.jpg')
+    	.then(function(upload) {
+    		// upload instanceof Client.Upload
+    		// nothing more than just keeping upload id
+    		console.log(upload.params.uploadId);
+    		return Client.Media.configurePhoto(session, upload.params.uploadId, 'akward caption');
+    	})
+    	.then(function(medium) {
+    		// we configure medium, it is now visible with caption
+    		console.log(medium.params)
+    	})
 
-    Instagram.getUserDataByUsername('realflorenzerstling').then((t) =>
-    {
-      console.log(t)
-      Instagram.getUserFollowers(JSON.parse(t).user.id).then((t) =>
-      {
-        console.log(t); // - instagram followers for user "username-for-get"
-      })
-    })
+    // Instagram.getUserDataByUsername('realflorenzerstling').then((t) =>
+    // {
+    //   // console.log(t)
+    //   Instagram.getUserFollowers(JSON.parse(t).user.id).then((t) =>
+    //   {
+    //     // console.log(t); // - instagram followers for user "username-for-get"
+    //   })
+    // })
 
   })
 })
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
