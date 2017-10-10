97dccec92286e41f89b10f69554638e93db9bdbc
Floreny <floreny@IES-loaner01.local>
2017-10-10 06:41:39 +0200

    added some functions

--- py/editImage.py
+++ py/editImage.py
@@ -3,23 +3,30 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import textwrap
 
-img = Image.open("../img/white.jpg")
-draw = ImageDraw.Draw(img)
+def readImage(imgPath):
+    img = Image.open("../img/white.jpg")
+    draw = ImageDraw.Draw(img)
+    return (img, draw)
+
+def createImageWithText(img, draw, imgPath, msg):
+    font = ImageFont.truetype("Cubano-Regular.otf", 45)
+    W, H = (1080,1080)
+    multi = 1080 / 300
+    msg = msg.upper()
+    msg = textwrap.fill(msg,30)
+    w, h = draw.textsize(msg)
+    new_w = W-w*multi
+    draw.multiline_text(((W-w*multi)/2,(H/3)), msg, fill='black', font=font, anchor=10, spacing=10, align="center")
+    index = 10
+    img.save('../img/image_ready_%d.jpg' % index)
+    return img
+
+
 # font = ImageFont.truetype(<font-file>, <font-size>)
-font = ImageFont.truetype("Cubano-Regular.otf", 40)
-W, H = (1080,1080)
-multi = 1080 / 300
+index = 10
+imagePath = "../img/white.jpg"
+saveImagePath = '../img/image_ready_%d.jpg' % index
 msg = "Mentors are great,but some of the greatest mentor are no longer alive"
-msg = "Mentors aasdf asdf asdfasdfasdf asdf asd fasdfasdf"
-msg = msg.upper()
-msg = textwrap.fill(msg,30)
-w, h = draw.textsize(msg)
-new_w = W-w*multi
-print(new_w)
-print(w,h)
-print(W,H)
-draw.multiline_text(((W-w*multi)/2,(H/3)), msg, fill='black', font=font, anchor=10, spacing=10, align="center")
-# draw.text(((W-w)/2,(H-h)/2), msg, fill="black",font=font)
-# draw.text((x, y),"Sample Text",(r,g,b))
-img.save('../img/output.jpg')
+(img, draw) = readImage(imagePath)
+img = createImageWithText(img, draw, saveImagePath, msg)
 img.show()
