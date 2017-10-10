a87f69128bda2c53276b3b73b4d722fea427de31
Floreny <floreny@IES-loaner01.local>
2017-10-10 06:57:22 +0200

    images of array are created

--- py/editImage.py
+++ py/editImage.py
@@ -8,7 +8,7 @@ def readImage(imgPath):
     draw = ImageDraw.Draw(img)
     return (img, draw)
 
-def createImageWithText(img, draw, imgPath, msg):
+def createImageWithText(img, draw, imgPath, msg,index):
     font = ImageFont.truetype("Cubano-Regular.otf", 45)
     W, H = (1080,1080)
     multi = 1080 / 300
@@ -17,24 +17,28 @@ def createImageWithText(img, draw, imgPath, msg):
     w, h = draw.textsize(msg)
     new_w = W-w*multi
     draw.multiline_text(((W-w*multi)/2,(H/3)), msg, fill='black', font=font, anchor=10, spacing=10, align="center")
-    index = 10
     img.save('../img/image_ready_%d.jpg' % index)
     return img
 
-def createAllImages(text):
-    index = 10
+def createAllImages(text,index):
     imagePath = "../img/white.jpg"
     saveImagePath = '../img/image_ready_%d.jpg' % index
     msg = "Mentors are great,but some of the greatest mentor are no longer alive"
     (img, draw) = readImage(imagePath)
-    img = createImageWithText(img, draw, saveImagePath, text)
+    img = createImageWithText(img, draw, saveImagePath, text, index)
     img.show()
+
+def start(stringArray):
+    for i in range (0,len(stringArray)):
+        createAllImages(stringArray[i],i)
+
+
 
 quotes = [
     "Mentors are great,but some of the greatest mentor are no longer alive1",
     "Mentors are great,but some of the greatest mentor are no longer alive2",
     "Mentors are great,but some of the greatest mentor are no longer alive3",
 ]
-createAllImages(quotes[0])
+start(quotes)
 
 # font = ImageFont.truetype(<font-file>, <font-size>)
