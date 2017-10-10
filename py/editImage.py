dc4c4bbf0e590d282a80b7baab5df01e2f4298cf
Floreny <floreny@IES-loaner01.local>
2017-10-10 06:13:57 +0200

    text almost located in the middle

--- py/editImage.py
+++ py/editImage.py
@@ -6,13 +6,20 @@ import textwrap
 img = Image.open("../img/white.jpg")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
-font = ImageFont.truetype("Arial.ttf", 30)
+font = ImageFont.truetype("Cubano-Regular.otf", 40)
 W, H = (1080,1080)
-msg = "Mentors are great, but some of the greatest mentors are no longer alive"
+multi = 1080 / 300
+msg = "Mentors are great,but some of the greatest mentor are no longer alive"
+msg = "Mentors aasdf asdf asdfasdfasdf asdf asd fasdfasdf"
 msg = msg.upper()
-msg = textwrap.fill(msg,20)
+msg = textwrap.fill(msg,30)
 w, h = draw.textsize(msg)
-draw.text(((W-w)/2,(H-h)/2), msg, fill="black",font=font)
+new_w = W-w*multi
+print(new_w)
+print(w,h)
+print(W,H)
+draw.multiline_text(((W-w*multi)/2,(H/3)), msg, fill='black', font=font, anchor=10, spacing=10, align="center")
+# draw.text(((W-w)/2,(H-h)/2), msg, fill="black",font=font)
 # draw.text((x, y),"Sample Text",(r,g,b))
 img.save('../img/output.jpg')
 img.show()
