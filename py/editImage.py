92bc809bf1efdf01949d372bf6590d8aea18c7fd
Floreny <floreny@IES-loaner01.local>
2017-10-12 07:53:00 +0200

    quotes are more beautiful

--- py/editImage.py
+++ py/editImage.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -9,14 +12,16 @@ def readImage(imgPath):
     return (img, draw)
 
 def createImageWithText(img, draw, imgPath, msg, fontColor):
-    font = ImageFont.truetype("Cubano-Regular.otf", 45)
+    # font = ImageFont.truetype("Cubano-Regular.otf", 45)
+    font = ImageFont.truetype("Roboto-BlackItalic.ttf", 60)
     W, H = (1080,1080)
     multi = 1080 / 300
     msg = msg.upper()
-    msg = textwrap.fill(msg,30)
+    msg = textwrap.fill(msg,20)
     w, h = draw.textsize(msg)
     new_w = W-w*multi
-    draw.multiline_text(((W-w*multi)/2,(H/3)), msg, fill=fontColor, font=font, anchor=10, spacing=10, align="center")
+    # draw.multiline_text(((W-w*multi)/2,(H/3)), msg, fill=fontColor, font=font, anchor=10, spacing=10, align="center")
+    draw.multiline_text(((W-w*5)/2,(H/3)), msg, fill=fontColor, font=font, anchor=10, spacing=15, align="center")
     img.save(imgPath)
     return img
 
@@ -31,23 +36,29 @@ def createSpecificImage(text,index,color):
     else:
         color = "white"
     img = createImageWithText(img, draw, saveImagePath, text, color)
-    img.show()
+    # img.show()
 
 
 def createAllImages(text,index):
     createSpecificImage(text,index,"white")
     createSpecificImage(text,index,"black")
+    createSpecificImage(text,index,"pic")
 
 def start(stringArray):
     for i in range (0,len(stringArray)):
         createAllImages(stringArray[i],i)
 
+def readQuotesFromFile(filename):
+    with open(filename) as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    content = [x.strip() for x in content]
+    return content
 
 
-quotes = [
-    "Mentors are great,but some of the greatest mentor are no longer alive1",
-    "Mentors are great,but some of the greatest mentor are no longer alive2"
-]
+fileName = 'quotes.json'
+quotes = readQuotesFromFile(fileName)
+
 start(quotes)
 
 # font = ImageFont.truetype(<font-file>, <font-size>)
---
+++ py/readFile.py
@@ -0,0 +1,10 @@
+def readQuotesFromFile(filename):
+    with open(filename) as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    content = [x.strip() for x in content]
+    return content
+
+
+file = 'quotes.json'
+readQuotesFromFile(file)
